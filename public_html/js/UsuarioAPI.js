b9a50a723d02b5f325d1637ce624b11f940e7ce7
MILENA CAPRARO <milenacapraro@hotmail.com>
2020-09-24 21:53:24 -0300

    Commit
    Commit

---
+++ public_html/js/UsuarioAPI.js
@@ -0,0 +1,33 @@
+var url = "http://localhost:8000";
+
+function getUsuarios(){
+    var data = new FormData();
+    var xhr = new XMLHttpRequest;
+    //xhr.withCredentials = true;
+    
+    xhr.addEventListener("readystatechange", function (){
+        if (this.readyState === 4){
+            var usuarios = JSON.parse(this.responseText);
+            //Faça algo
+            exibirUsuarios(usuarios);
+        }
+    });
+    
+    xhr.open("GET", url);
+    xhr.send(data);
+}
+
+function adicionarUsuario(objUsuario){
+    var xhr = new XMLHttpRequest();
+    
+    xhr.addEventListener("readystatechange", function(){
+        if (this.readyState === 4){
+            var usuarios = JSON.parse(this.responseText);
+            //Faça algo
+            alert("Usuário adicionado");
+        }
+    });
+    
+    xhr.open("POST", url);
+    xhr.send(JSON.stringify(objUsuario));
+}
---
+++ public_html/js/UsuarioView.js
@@ -0,0 +1,89 @@
+function exibirUsuarios(usuarios) {
+
+    popularTabela(usuarios);
+}
+
+function popularTabela(usuarios) {
+    for (var i = 0; i < usuarios.length; i++) {
+        inserirLinhaTabela(usuarios[i]);
+    }
+}
+
+function inserirLinhaTabela(usuario) {
+    var tabela = document.getElementById('tabelaDeUsuarios');
+    var numLinhas = tabela.rows.length;
+    var novaLinha = tabela.insertRow(numLinhas);
+
+    var celCodigo = novaLinha.insertCell(0);
+    celCodigo.innerHTML = usuario.codigo;
+
+    var celNome = novaLinha.insertCell(1);
+    celNome.innerHTML = usuario.nome;
+
+    var celEmail = novaLinha.insertCell(2);
+    celEmail.innerHTML = usuario.email;
+
+    var celLogin = novaLinha.insertCell(3);
+    celLogin.innerHTML = usuario.login;
+
+}
+
+function validarUsuario(acao) {
+    var nome = document.getElementById('nome');
+    var email = document.getElementById('email');
+    var login = document.getElementById('login');
+    var senha = document.getElementById('senha');
+    var senhaValidar = document.getElementById('senhaValidar');
+
+    if (acao == "add") {
+        var dadosValidos = true;
+
+        if (nome.value == "") {
+            dadosValidos = false;
+            alert("Preencha o campo nome");
+        } else if (email.value == "") {
+            dadosValidos = false;
+            alert("Informe um e-mail");
+        } else if (login.value == "") {
+            dadosValidos = false;
+            alert("Preencha o campo login");
+        } else {
+            if (senha.value != "") {
+                if (senha.value == senhaValidar.value) {
+                    var senhaTxt = senha.value;
+
+                    if (senhaTxt.length < 6) {
+                        dadosValidos = false;
+                        alert("A senha precisa ter pelo menos 6 caracteres");
+                    }
+                } else {
+                    dadosValidos = false;
+                    alert("As senhas informadas não são iguais");
+                }
+
+            } else {
+                dadosValidos = false;
+                alert("Informe a senha");
+            }
+        }
+
+        if (dadosValidos) {
+            //Enviar para a API
+            var objUsuario = {
+                "nome": nome.value,
+                "email": email.value,
+                "login": login.value,
+                "senha": senha.value
+            };
+            //Adiciona o novo usuário
+            adicionarUsuario(objUsuario);
+        }
+
+    }
+
+    return false;
+}
+
+window.onload = function () {
+    getUsuarios();
+};
